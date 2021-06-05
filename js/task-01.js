bff88dafcecac980b5381bd4d2df72d244ff0245
Артур Евсеев <75883199+ArmatuRio@users.noreply.github.com>
2021-06-05 09:16:14 +0300

    исправлена задача 1 и 6

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,32 @@
-"use strict";
 
-const totalCategories = document.querySelectorAll(".item");
-console.log(`В списке ${totalCategories.length} категории.`);
+// ------решение 1-----
 
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Категория: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
 
+// const totalCategories = document.querySelectorAll(".item");
+// console.log(`В списке ${totalCategories.length} категории.`);
+
+// const categoriesArray = [...totalCategories]
+//   .map(
+//     categories => `Категория: ${categories.children[0].textContent}
+// Количество элементов: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesArray);
+
+// ------решение 2-----
+
+const itemlength = (function () {
+  const item = document.querySelectorAll('.item');
+  return console.log(`В списке ${item.length} категории.`);
+})();
+
+const ulCategories = document.querySelector('#categories');
+
+const elemUl = ulCategories.querySelectorAll('.item');
+
+elemUl.forEach((elem) => {
+  console.log(`-Категория: ${elem.firstElementChild.textContent}`);
+  console.log(
+    `-Количество элементов: ${elem.lastElementChild.children.length}`
+  );
+});
--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,35 @@
-let inputVal = document.getElementById("validation-input");
+// ------решение 2-----
 
-let totalLenght = inputVal.getAttribute("data-length");
-let intTotallenght = parseInt(totalLenght, 10);
+// let inputVal = document.getElementById("validation-input");
 
-inputVal.oninput = function() {
-  if (inputVal.value.length === intTotallenght) {
-    inputVal.classList.remove("invalid");
-    inputVal.classList.add("valid");
+// let totalLenght = inputVal.getAttribute("data-length");
+// let intTotallenght = parseInt(totalLenght, 10);
+
+// inputVal.oninput = function() {
+//   if (inputVal.value.length === intTotallenght) {
+//     inputVal.classList.remove("invalid");
+//     inputVal.classList.add("valid");
+//   }
+//   if (inputVal.value.length === 0) {
+//     inputVal.classList.remove("valid");
+//     inputVal.classList.remove("invalid");
+//   }
+//   if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+//     inputVal.classList.add("invalid");
+//   }
+// };
+
+// ------решение 2-----
+
+const validationIn = document.getElementById('validation-input');
+Number(validationIn.dataset.length);
+
+validationIn.addEventListener('blur', (elem) => {
+  if (Number(validationIn.dataset.length) !== elem.currentTarget.value.length) {
+    validationIn.classList.add('invalid');
+    validationIn.classList.remove('valid');
+  } else {
+    validationIn.classList.add('valid');
+    validationIn.classList.remove('invalid');
   }
-  if (inputVal.value.length === 0) {
-    inputVal.classList.remove("valid");
-    inputVal.classList.remove("invalid");
-  }
-  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-    inputVal.classList.add("invalid");
-  }
-};
+});
